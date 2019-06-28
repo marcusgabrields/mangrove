048f16ae5ce11e341f1b23a349828048326fd7ee
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2019-06-27 23:02:11 -0300

    Add manager and absolute url

--- mangrove/blog/models.py
+++ mangrove/blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from markdownx.models import MarkdownxField
+from django.urls import reverse
 
 from mangrove.common.models import IndexedTimeStampedModel
+from .managers import FinishedsArticleManager
 
 
 class Tag(IndexedTimeStampedModel):
@@ -16,8 +18,8 @@ class Tag(IndexedTimeStampedModel):
 
 class Article(IndexedTimeStampedModel):
 
-    title = models.CharField(_('title'), max_length=100)
-    slug = models.SlugField(_('slug'), unique=True)
+    title = models.CharField(_('title'), max_length=255)
+    slug = models.SlugField(_('slug'), unique=True, max_length=255)
     content = MarkdownxField(_('content'), blank=True)
     preview = models.TextField(_('preview'), blank=True)
     date = models.DateField(_('date'), blank=True, null=True)
@@ -26,5 +28,10 @@ class Article(IndexedTimeStampedModel):
     tags = models.ManyToManyField('blog.Tag')
     draft = models.BooleanField(_('draft'), default=True)
 
+    finisheds = FinishedsArticleManager()
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('article', kwargs={'slug': self.slug})
