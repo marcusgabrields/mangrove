2eec2ff7fefb7763caa9c5538ccdbf0193669a0f
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2019-06-27 23:13:09 -0300

    Add core app

--- mangrove/settings.py
+++ mangrove/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'test_without_migrations',
     'markdownx',
+    'mangrove.core',
     'mangrove.account',
     'mangrove.common',
     'mangrove.blog',
